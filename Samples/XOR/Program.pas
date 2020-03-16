89885af547cc9f150c31b4a51b3536db913735f6
arcturus5340 <arcturus5340@gmail.com>
2020-03-16 15:24:54 +0300

    Added Sample for trainig a neural network XOR operation

---
+++ Samples/XOR/Program.pas
@@ -0,0 +1,30 @@
+﻿uses nntk, vector_math;
+
+const
+  LEARNING_RATE = 0.15;
+  EPOCHS_COUNT = 500;
+  DROPOUT_PROBABILITY = 0.0;
+
+begin
+  var ipt := new List<vector_math.Vector>;
+  ipt.add(new Vector(0, 0));
+  ipt.Add(new Vector(0, 1));
+  ipt.Add(new Vector(1, 0));
+  ipt.Add(new Vector(1, 1));
+  var opt := new List<vector>;
+  opt.Add(new Vector(0));
+  opt.Add(new Vector(1)); 
+  opt.Add(new Vector(1));
+  opt.Add(new Vector(0));
+
+  var size := new Vector(2, 4, 1); 
+  var nn := new Neural_Network(size);
+  nn.train(ipt, opt, 
+           EPOCHS_COUNT, 
+           LEARNING_RATE, 
+           nntk.Functions.tanh, 
+           nntk.Functions.tanh_derivative, 
+           DROPOUT_PROBABILITY);
+  
+  println('Neural Network output for (0, 1): ', nn.run(new Vector(0, 1)));  
+end.
