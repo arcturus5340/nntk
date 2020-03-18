58c9f37866cc262800863935f64ca413903502d9
arcturus5340 <arcturus5340@gmail.com>
2020-03-18 18:53:03 +0300

    Updated Samples

--- Samples/MNIST/Program.pas
+++ Samples/MNIST/Program.pas
@@ -1,9 +1,18 @@
 ﻿uses nntk, vector_math;
 
 const
-  LEARNING_RATE = 0.005;
-  EPOCHS_COUNT = 300;
+  LEARNING_RATE = 0.0015;
+  EPOCHS_COUNT = 200;
   DROPOUT_PROBABILITY = 0.5;
+  BATCH_SIZE = 100;
+  SEED = 0;
+  
+var
+  INITIALIZING_WEIGHTS_RANGE: System.Tuple<real, real> := (-1.0, 1.0);
+  ACTIVATION_FUNCTIONS: array of nntk.functions_type := 
+  (nntk.functions.tanh, nntk.functions.softmax);
+  ACTIVATION_FUNCTIONS_DERIVATIVES: array of nntk.functions_type := 
+  (nntk.functions.tanh_derivative, nntk.functions.softmax_derivative);
 
 begin
   var ipt := new List<Vector>;
@@ -16,42 +25,43 @@ begin
   end; 
   var opt := new list<Vector>;
   foreach var str in ReadallLines('labels.txt') do
-  begin  
+  begin
     var x := new Vector;
     foreach var num in str.Split(' ') do
       x.push_back(real.Parse(num));
     opt.Add(x);
   end; 
-  
   var size := new Vector(784, 100, 10); 
-  var nn := new Neural_Network(size);
+  var nn := new Neural_Network(size, 
+                               INITIALIZING_WEIGHTS_RANGE, 
+                               ACTIVATION_FUNCTIONS, 
+                               ACTIVATION_FUNCTIONS_DERIVATIVES,
+                               SEED);
   nn.train(ipt, opt, 
            EPOCHS_COUNT, 
            LEARNING_RATE, 
-           nntk.Functions.relu, 
-           nntk.Functions.relu_derivative, 
-           DROPOUT_PROBABILITY);
+           DROPOUT_PROBABILITY,
+           BATCH_SIZE);
   
-  var my_model := nn.get_model();
-  ipt := new List<Vector>;
-  foreach var str in ReadallLines('test_images.txt') do
-  begin  
-    var x := new Vector;
-    foreach var num in str.Split(' ') do
-      x.push_back(real.Parse(num));
-    ipt.Add(x);
-  end;
-  opt := new List<Vector>;
-  foreach var str in ReadallLines('test_labels.txt') do
-  begin  
-    var x := new Vector;
-    foreach var num in str.Split(' ') do
-      x.push_back(real.Parse(num));
-    opt.Add(x);
-  end; 
-  var error := 0.0;
-  println(ipt.Count);
-  for var index := 0 to ipt.Count-1 do 
-    error += ((my_model(ipt[index]) - opt[index]) ** 2).sum();
-  println(error / ipt.Count);
+//  var my_model := nn.get_model();
+//  ipt := new List<Vector>;
+//  foreach var str in ReadallLines('test_images.txt') do
+//  begin  
+//    var x := new Vector;
+//    foreach var num in str.Split(' ') do
+//      x.push_back(real.Parse(num));
+//    ipt.Add(x);
+//  end;
+//  opt := new List<Vector>;
+//  foreach var str in ReadallLines('test_labels.txt') do
+//  begin  
+//    var x := new Vector;
+//    foreach var num in str.Split(' ') do
+//      x.push_back(real.Parse(num));
+//    opt.Add(x);
+//  end; 
+//  var error := 0.0;
+//  println(ipt.Count);
+//  for var index := 0 to ipt.Count-1 do 
+//    println('Error: ', ((my_model(ipt[index]) - opt[index]) ** 2).sum());
 end.
--- Samples/XOR/Program.pas
+++ Samples/XOR/Program.pas
@@ -1,9 +1,25 @@
 ﻿uses nntk, vector_math;
 
 const
+  EPOCHS_COUNT = 100;
   LEARNING_RATE = 0.15;
-  EPOCHS_COUNT = 500;
   DROPOUT_PROBABILITY = 0.0;
+  BATCH_SIZE = 1;
+  SEED = 0;
+
+var
+  INITIALIZING_WEIGHTS_RANGE: System.Tuple<real, real> := (-10.0, 10.0);
+  ACTIVATION_FUNCTIONS: array of nntk.functions_type := 
+  (
+    nntk.functions.tanh,
+    nntk.functions.tanh
+  );
+  ACTIVATION_FUNCTIONS_DERIVATIVES: array of nntk.functions_type := 
+  (
+    nntk.functions.tanh_derivative, 
+    nntk.functions.tanh_derivative
+  );
+
 
 begin
   var ipt := new List<vector_math.Vector>;
@@ -18,13 +34,16 @@ begin
   opt.Add(new Vector(0));
 
   var size := new Vector(2, 4, 1); 
-  var nn := new Neural_Network(size);
+  var nn := new Neural_Network(size,
+                               INITIALIZING_WEIGHTS_RANGE,
+                               ACTIVATION_FUNCTIONS,
+                               ACTIVATION_FUNCTIONS_DERIVATIVES,
+                               SEED);
   nn.train(ipt, opt, 
            EPOCHS_COUNT, 
            LEARNING_RATE, 
-           nntk.Functions.tanh, 
-           nntk.Functions.tanh_derivative, 
-           DROPOUT_PROBABILITY);
-  
-  println('Neural Network output for (0, 1): ', nn.run(new Vector(0, 1)));  
+           DROPOUT_PROBABILITY,
+           BATCH_SIZE);
+           
+  println('Neural Network output for (1, 0): ', nn.run(new Vector(1, 0)));  
 end.
